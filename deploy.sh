9e5329ca8877aed2f7bdb0fe89829fe982e5231e
Tim Crall <timc@ignw.io>
2018-11-26 14:16:07 -0800

    Added stuff from packer branch

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+function timestamp {
+  echo $(date "+%F %T")
+}
+
+function info {
+  echo "$(timestamp) INFO:  $1"
+}
+
+function error {
+  echo "$(timestamp) ERROR: $1"
+}
+
 if [ $# -lt 1 ];
     then echo "Usage: deploy.sh <cluster>"
     exit 1
@@ -7,6 +19,36 @@ fi
 
 CLUSTER=$1
 
-echo "Deploying Docker Enterprise Edition on cluster ${CLUSTER}"
-terraform init -backend-config="settings/${CLUSTER}/${CLUSTER}.init"
-terraform apply -auto-approve -var-file "settings/${CLUSTER}/${CLUSTER}.tfvars"
+if [ -z $PACKER_PATH ]; then
+  PACKER_PATH="/usr/local/packer"
+fi
+
+if [ -z $PACKER_IMAGE ]; then
+  PACKER_IMAGE="images/dockeree-centos.json"
+fi
+
+info "Deploying Docker Enterprise Edition on cluster ${CLUSTER}"
+
+info "Creating disk image with Packer"
+${PACKER_PATH} build ${PACKER_IMAGE}
+result=$?
+if [ $result -ne 0 ]; then
+  error "Packer build failed"
+  exit 1
+fi
+
+info "Initializing terraform"
+terraform init -backend-config="clusters/${CLUSTER}/${CLUSTER}.init"
+result=$?
+if [ $result -ne 0 ]; then
+  error "Terraform initalization faled"
+  exit 1
+fi
+
+info "Applying terraform changes"
+terraform apply -auto-approve -var-file "clusters/${CLUSTER}/${CLUSTER}.tfvars"
+result=$?
+if [ $result -ne 0 ]; then
+  error "Terraform apply failed"
+  exit 1
+fi
