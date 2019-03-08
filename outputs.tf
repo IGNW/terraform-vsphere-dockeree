62c040bee87851f9480282e495274224fd89fbb2
Tim Crall <timcrall@gmail.com>
2019-03-08 15:03:34 -0800

    Ok, suppressing output still doesn't allow us to not spin up all node types - reverting

--- outputs.tf
+++ outputs.tf
@@ -2,22 +2,22 @@ output "manager_ips" {
   value = "${module.docker-manager.node_ips}"
 }
 
-#output "worker_ips" {
-#  value = "${module.docker-worker.node_ips}"
-#}
+output "worker_ips" {
+  value = "${module.docker-worker.node_ips}"
+}
 
-#output "dtr_ips" {
-#  value = "${module.docker-dtr.node_ips}"
-#}
+output "dtr_ips" {
+  value = "${module.docker-dtr.node_ips}"
+}
 
-#output "minio_ip" {
-#  value = "${module.minio.public_ip}"
-#}
+output "minio_ip" {
+  value = "${module.minio.public_ip}"
+}
 
 output "ucp_url" {
   value = "https://${module.docker-manager.node_ips[0]}"
 }
 
-#output "dtr_url" {
-#  value = "https://${module.docker-dtr.node_ips[0]}"
-#}
+output "dtr_url" {
+  value = "https://${module.docker-dtr.node_ips[0]}"
+}
